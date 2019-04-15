b37b3fc5e2b4117fd063c83fbd8974368f5cf4c2
Ycheck <17326842024@163.com>
2019-04-15 20:56:59 +0800

    config修改

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,25 @@ const path = require('path')
 // 去console插件
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const CompressionWebpackPlugin = require('compression-webpack-plugin')
+// 分析打包体积用
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const resolve = dir => path.resolve(__dirname, dir)
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   outputDir: './dist',
   lintOnSave: true,
+  // 关闭sourceMap
+  productionSourceMap: false,
   // 是否提取css到style标签
   // css: {
   //   extract: false
   // },
   chainWebpack: (config) => {
+    // 移除 prefetch 插件
+    /**
+     * vue-cli3.0默认会在用户空闲时提前预加载一些将来可能会用到的资源，在移动端会消耗带宽，需要去掉
+     */
+    config.plugins.delete('prefetch')
     // 这里配置px2rem-loader
     config.module
       .rule('less')
@@ -38,7 +47,7 @@ module.exports = {
             drop_console: true
           }
         },
-        sourceMap: true,
+        sourceMap: false,
         parallel: true
       }),
       new CompressionWebpackPlugin({
@@ -52,6 +61,7 @@ module.exports = {
         threshold: 10240,
         minRatio: 0.8
       })
+      // new BundleAnalyzerPlugin()
     ]
     if (process.env.NODE_ENV !== 'development') {
       config.plugins = [...config.plugins, ...plugins]
