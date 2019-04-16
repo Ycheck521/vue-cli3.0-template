169596585eeac84f0043b6f39815b8b46be65dda
Ycheck <17326842024@163.com>
2019-04-16 16:55:45 +0800

    资源加载更改

--- src/config/cdn.js
+++ src/config/cdn.js
@@ -3,8 +3,6 @@ module.exports = {
     // 开发环境
     dev: {
       css: [
-        'https://unpkg.com/element-ui/lib/theme-chalk/index.css',
-        'https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.css'
       ],
       js: []
     },
@@ -15,9 +13,9 @@ module.exports = {
         'https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.css'
       ],
       js: [
-        'https://cdn.bootcss.com/vue/2.5.21/vue.min.js',
-        'https://cdn.bootcss.com/vue-router/3.0.2/vue-router.min.js',
-        'https://cdn.bootcss.com/vuex/3.0.1/vuex.min.js',
+        'https://cdn.bootcss.com/vue/2.6.10/vue.min.js',
+        'https://cdn.bootcss.com/vue-router/3.0.4/vue-router.min.js',
+        'https://cdn.bootcss.com/vuex/3.1.0/vuex.min.js',
         'https://cdn.bootcss.com/axios/0.18.0/axios.min.js',
         'https://unpkg.com/element-ui/lib/index.js',
         'https://cdn.bootcss.com/js-cookie/2.2.0/js.cookie.min.js',
